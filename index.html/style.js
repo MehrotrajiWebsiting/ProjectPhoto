78de7a9da1b76440adddf933c3699e5f801cb407
unknown <cocroar069@gmail.com>
2022-05-25 22:22:43 +0530

    CAMERA AND TEXT ADDED

--- index.html/style.js
+++ index.html/style.js
@@ -6,11 +6,26 @@ window.addEventListener("DOMContentLoaded",function (){let width = window.innerW
     document.getElementById("BAR").addEventListener("click",dropdown,false);
 }
 else{
-    var a='<a href="#about">About</a><a href="#contact">Contact</a><a href="#news">News</a><a class="active" href="#home">Home</a>';
+    var a='<a href="#about">About us</a><a href="#contact">Contact us</a><a href="#news">Our works</a><a class="active" href="#home">Home</a>';
     document.querySelector(".topnav").innerHTML=a;
 }  
 function dropdown(){
-        a='<br><div><a class="active" href="#home">Home</a></div><div><a href="#news">News</a></div><div><a href="#contact">Contact</a></div><div><a href="#about">About</a></div>';
+        a='<br><div id="dhome"><a class="active" href="#home">Home</a></div><div id="dnews"><a href="#news">Our works</a></div><div id="dcontact"><a href="#contact">Contact us</a></div><div id="dabout"><a href="#about">About us</a></div>';
         document.querySelector("#drop").innerHTML=a;
+        document.getElementById("dhome").addEventListener("click",dropdownclose,false);
+        document.getElementById("dnews").addEventListener("click",dropdownclose,false);
+        document.getElementById("dcontact").addEventListener("click",dropdownclose,false);
+        document.getElementById("dabout").addEventListener("click",dropdownclose,false);
     }
+function dropdownclose(){
+    document.querySelector("#drop").innerHTML='';
+}
+window.addEventListener('click', function(e){   
+    if (document.querySelector(".topnav").contains(e.target)){
+      // Clicked in box
+    } else{
+      // Clicked outside the box
+      dropdownclose();
+    }
+  });    
 });  
